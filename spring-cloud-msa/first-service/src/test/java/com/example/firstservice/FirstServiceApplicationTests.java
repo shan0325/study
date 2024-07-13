65b2adf727077a9c615f29f162bc1423c8c7054c
shan0325 <shan0325@naver.com>
2024-07-13 14:53:32 +0900

    add project docker-react-app

---
+++ spring-cloud-msa/first-service/src/test/java/com/example/firstservice/FirstServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.firstservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServiceApplicationTests {
+
+}
